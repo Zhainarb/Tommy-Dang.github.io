605f01186f66d5d854d648ec43546de5787cf312
Tommy Dang <tommydang@Tommys-MacBook-Air.local>
2016-11-29 21:09:33 -0600

    Update contours

--- WaterLevel/Contour/js/main.js
+++ WaterLevel/Contour/js/main.js
@@ -1,7 +1,7 @@
 var data2D = [];
       
 d3.tsv("data/ascii_2013.csv", function(error, data_) { 
-  for (var i=0;i<data_.length-100;i++){
+  for (var i=0;i<data_.length-19;i++){
     var a=[];
     for (var key in data_[i]){
       var v = +data_[i][key];
@@ -12,13 +12,13 @@ d3.tsv("data/ascii_2013.csv", function(error, data_) {
     }
     data2D.unshift(a);
   }
+  console.log(data_.length);
 
   var data = [ {
     z: data2D,  
  //  zsmooth: 'best',
   type: 'contour',
   //colorscale: 'Jet',
-  colorscale: [[0, 'rgba(255,255,255,0)'], [0.1, 'rgba(31,120,180,255)'], [0.25, 'rgb(178,200,138)'], [0.4, 'rgb(151,160,44)'], [0.55, 'rgb(251,154,153)'], [0.7, 'rgb(227,26,28)'] , [0.85, 'rgb(150,0,0)'] , [1, 'rgb(110,0,0)']],
   showscale: true,
   autocontour: false,
   contours: {
@@ -26,6 +26,7 @@ d3.tsv("data/ascii_2013.csv", function(error, data_) {
     end: 900,
     size: 80
   },
+  colorscale: [[0, 'rgba(255,255,255,0)'], [0.1, 'rgba(31,120,180,255)'], [0.25, 'rgb(178,200,138)'], [0.4, 'rgb(151,160,44)'], [0.55, 'rgb(251,154,153)'], [0.7, 'rgb(227,26,28)'] , [0.85, 'rgb(150,0,0)'] , [1, 'rgb(110,0,0)']],
   xaxis: 'x3',
   yaxis: 'y3'
    }];
@@ -38,3 +39,4 @@ d3.tsv("data/ascii_2013.csv", function(error, data_) {
 
   Plotly.newPlot('myDiv', data, layout);
 });
+
